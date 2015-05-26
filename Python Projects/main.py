60134a49e9f20d7a7920d24cf2deba4b4350d703
Graham Dominick <Graham@Grahams-MacBook-Pro.local>
2015-05-26 12:47:07 -0400

    Deleted Test File

--- Python Projects/main.py
+++ Python Projects/main.py
@@ -1,16 +1,11 @@
 __author__ = 'Graham'
-import Test
+import sys
+sys.path.append('/Users/Graham/Desktop/biopython')
 from Bio import Seq
 from Bio.Alphabet import IUPAC
 from Bio import SeqIO
+from Bio.Data import IUPACData
 
-infile = list(SeqIO.parse('rosalind_grph.txt', 'fasta'))
-list_edges = []
-for fragment in infile:
-    for other_fragment in infile:
-        if fragment.seq[-3:] == other_fragment.seq[0:3] and fragment != other_fragment:
-            list_edges.append(str(fragment.id + ' ' + other_fragment.id))
 
-for entry in list_edges:
-        print entry
+
 
