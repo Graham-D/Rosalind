ede0e613152326d17e97b13788887cac321fafbd
Graham Dominick <Graham@Grahams-MacBook-Pro.local>
2015-05-31 22:48:20 -0600

    Error Correction

--- Error_Correction.py
+++ Error_Correction.py
@@ -1,5 +1,6 @@
 __author__ = 'Graham'
 
+from Hamming_Dist import hamming_dist
 
 def correct(inlist):
     sequence_list = []
@@ -9,27 +10,15 @@ def correct(inlist):
         sequence_list.append(read.seq)
     for sequence in sequence_list:
         if sequence_list.count(sequence) == 1:
-            if sequence.reverse_complement() not in sequence_list:
-                loners.append(sequence_list.pop(sequence_list.index(sequence)))
+            loners.append(sequence_list[sequence_list.index(sequence)])
+    for sequence in loners:
+        sequence_list.remove(sequence)
     for sequence in loners:
         for other_sequence in sequence_list:
             if hamming_dist(sequence, other_sequence) == 1:
-                print sequence + '->' + other_sequence
-                error_list.append(sequence + '->' + other_sequence)
+                error_list.append(str(sequence + '->' + other_sequence))
                 break
             elif hamming_dist(sequence, other_sequence.reverse_complement()) == 1:
-                print sequence + '->' + other_sequence.reverse_complement()
-                error_list.append(sequence + '->' + other_sequence)
+                error_list.append(str(sequence + '->' + other_sequence.reverse_complement))
                 break
-    for item in error_list:
-        if error_list.count(item) > 1:
-            print 'X' * 50
-
-
-def hamming_dist(seq1,seq2):
-    distance = 0
-    for base in range(len(seq1)):
-        if seq1[base] != seq2[base]:
-            distance += 1
-    return distance
-
+    return error_list
